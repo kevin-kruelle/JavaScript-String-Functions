43f3a77c029f0f1985d41fa68b35d14757394e66
Kevin Kruelle <kkruelle2@gmail.com>
2021-02-15 15:21:39 -0500

    finished activity 1 for functions

--- js/function-strings.js
+++ js/function-strings.js
@@ -1,1 +1,15 @@
 console.log('connected...')
+
+
+
+function camelCase(string) {
+    let newString = '';
+    let words = string.split(' ');
+    for(let i = 0; i < words.length; i++) {
+       words[i] = words[i].charAt(0).toUpperCase() + words[i].substr(1);
+    }
+    newString = words.join('');
+    return newString
+}
+
+console.log(camelCase('How are you'));
